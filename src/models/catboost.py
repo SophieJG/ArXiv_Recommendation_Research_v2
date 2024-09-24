f54340b753efd183ea823e2aaf0ff4834a4cc11f
Yotam-Elor <yotam.elor@gmail.com>
2024-09-24 19:14:59 -0400

    doc

--- src/models/catboost.py
+++ src/models/catboost.py
@@ -25,14 +25,17 @@ class CatboostModel(BaseModel):
         fold: str
     ):
         """
-Loads a fold and converts it to pandas dataframe
+Loads a fold and converts it to pandas dataframe. Some non-trivial data processing is required to convert the paper and author
+dictionaries to rows in a dataframe
 """
         samples = data.get_fold(fold)
-        # Copy fields from the data dictionaries 
         new_samples = []
         for sample in tqdm(samples, "Converting samples to dataframe"):
+            # Copy fields from the data dictionaries 
             new_sample = {key: sample[key] for key in ["title", "referenceCount", "categories", "label"]}
             new_sample["author_num_papers"] = len(sample["author"]["papers"])
+            # Go over the author papers and collect the fiefieldsOfStudy and s2FieldsOfStudy into two lists
+            # Additionally, the papers' titles are concatenated into a long string
             new_sample["author_fieldsOfStudy"] = []
             new_sample["author_s2FieldsOfStudy"] = []
             new_sample["author_title"] = ""
@@ -42,7 +45,7 @@ Loads a fold and converts it to pandas dataframe
                         new_sample[f"author_{key}"] += p[key]
                 if p["title"] is not None:
                     new_sample["author_title"] += " " + p["title"]
-            new_sample["is_cited"] = int(sample["author"]["id"]) in sample["cited_authors"]
+            new_sample["is_cited"] = int(sample["author"]["id"]) in sample["cited_authors"]  # Does the paper cites the author
             new_samples.append(new_sample)
         df = pd.DataFrame.from_records(new_samples)
         X = df[[col for col in df.columns if col != "label"]]
@@ -53,6 +56,11 @@ Loads a fold and converts it to pandas dataframe
         self,
         data: Data
     ):
+        """
+1. Fit the preprocessing pipeline on the training data
+2. Convert the training and validation data using the preprocessing pipeline
+3. Train the model on the processed training and validation data
+"""
         X_train, y_train = self.load_fold(data, "train")
         self.feature_processing_pipeline = ColumnTransformer([
             ('passthrough', 'passthrough', ["referenceCount", "author_num_papers", "is_cited"]),
@@ -65,10 +73,14 @@ Loads a fold and converts it to pandas dataframe
         print("Training data size:", X_train.shape, " type:", type(X_train))
         X_val, y_val = self.load_fold(data, "validation")
         X_val = self.feature_processing_pipeline.transform(X_val)
-        self.model = CatBoostClassifier()
-        self.model.fit(X_train, y_train, eval_set=[(X_val, y_val)], early_stopping_rounds=10, use_best_model=True)
+        self.model = CatBoostClassifier().fit(
+            X_train, y_train, eval_set=[(X_val, y_val)], early_stopping_rounds=10, use_best_model=True
+            )
 
     def predict_proba(self, data: Data, fold: str):
+        """
+Run inference on a fold
+"""
         assert self.model is not None
         X, _ = self.load_fold(data, fold)
         X = self.feature_processing_pipeline.transform(X)
